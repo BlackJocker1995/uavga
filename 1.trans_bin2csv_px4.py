ee9c6cac0c0f9d1af287741bce8a99a852ab48d5
hanruidong <han784851198@live.com>
2022-12-09 22:35:17 +0800

    remove wrong files

--- 1.trans_bin2csv_px4.py
+++
@@ -1,6 +0,0 @@
-from Cptool.config import toolConfig
-from Cptool.gaMavlink import GaMavlinkPX4
-
-if __name__ == '__main__':
-    toolConfig.select_mode("PX4")
-    GaMavlinkPX4.extract_log_path(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/ulg_changed", threat=6)
