a6589bb3e03558b828e9e9b0677d8af58fcc1568
rain <han784851198@live.com>
2024-10-12 16:22:03 +0800

    add comment

--- 2.extract_feature.py
+++ 2.extract_feature.py
@@ -5,8 +5,8 @@ from ModelFit.approximate import CyLSTM
 Train LSTM Model
 """
 if __name__ == '__main__':
-    # pd_csv = CyLSTM.merge_file_data(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/bin_ga/csv")
-    # CyLSTM.fit_trans(pd_csv)
+    pd_csv = CyLSTM.merge_file_data(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/bin_ga/csv")
+    CyLSTM.fit_trans(pd_csv)
 
     lstm = CyLSTM(100, 512)
     feature = lstm.extract_feature(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/bin_regular/csv")
--- 2.extract_feature_px4.py
+++ 2.extract_feature_px4.py
@@ -6,8 +6,8 @@ Train LSTM Model
 """
 if __name__ == '__main__':
     toolConfig.select_mode("PX4")
-    # pd_csv = CyLSTM.merge_file_data(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/ulg_changed/csv")
-    # CyLSTM.fit_trans(pd_csv)
+    pd_csv = CyLSTM.merge_file_data(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/ulg_changed/csv")
+    CyLSTM.fit_trans(pd_csv)
 
     lstm = CyLSTM(100, 512)
     feature = lstm.extract_feature(f"{toolConfig.ARDUPILOT_LOG_PATH}/logs/ulg_changed/csv")
